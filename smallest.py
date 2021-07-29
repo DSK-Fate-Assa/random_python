eb5f6e2b7f3267c2282ac2410db8e7ad44e3c99f
DSK-Fate-Assa <84035813+DSK-Fate-Assa@users.noreply.github.com>
2021-07-29 10:40:32 +0300

    Update smallest.py

--- smallest.py
+++ smallest.py
@@ -2,9 +2,15 @@ x=eval(input("enter first number   "))
 y=eval(input("enter second number   "))
 z=eval(input("enter third number   "))
 
+
+#method 1
 if (x<y) and (x<z):
 	print("the smallest number is ",x)
 elif (y<z) and (y<x):
 	print("the smallest number is ",y)
 else:
 	print("the smallest number is ",z)
+	
+#method2
+lst = [x, y, z]
+print("The smallest number is", min(lst))
