67ad2daf8db1ba37c1b3df8c798b4a53ff975c6e
DSK-Fate-Assa <84035813+DSK-Fate-Assa@users.noreply.github.com>
2021-07-29 10:33:04 +0300

    Delete clubcw1.py

--- clubcw1.py
+++
@@ -1,8 +0,0 @@
-x = eval(input("How much did you get in science    "))
-
-if (x < 50):
-    print("haaaaaahaha u failed, try again next time")
-elif (x < 80):
-    print("Nice try but you can do better")
-else:
-    print("noice u got an A or above, nerd")
