15c9a2a1c2997add25dea4337aa3b4e553bf423a
DSK-Fate-Assa <84035813+DSK-Fate-Assa@users.noreply.github.com>
2021-07-29 11:07:34 +0300

    Delete classwork.py

--- classwork.py
+++
@@ -1,9 +0,0 @@
-#to prompt the user to enter 2 numbers and print to say which one has a higher value
-x=eval(input("enter the first number    "))
-y=eval(input("enter the second number   "))
-if y<x:
-    print(x,"has a higher value than",y)
-elif y>x:
-    print(y,"has a higher value than",x)
-else:
-    print("both the numbers have the same value")
