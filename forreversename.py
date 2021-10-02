51f476c63750e045bce2c8f0fe380f80d8c1be34
DSK-Fate-Assa <84035813+DSK-Fate-Assa@users.noreply.github.com>
2021-10-02 20:13:55 +0300

    Delete forreversename.py

--- forreversename.py
+++
@@ -1,7 +0,0 @@
-#write a python program that inputs the users name backwards
-name = input("What is your name?  ")
-
-namex = len(name)
-print("Your name in reverse is ", end="")
-for x in reversed(name):
-	print(x, end="")
