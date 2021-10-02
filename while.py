b2750fef8369e3de7e2039051df60795d2ba1aba
DSK-Fate-Assa <84035813+DSK-Fate-Assa@users.noreply.github.com>
2021-10-02 19:32:20 +0300

    Delete while.py

--- while.py
+++
@@ -1,8 +0,0 @@
-count = 10
-while (count >= 0):
-	print("the count right now is: ", count)
-	count = count-1
-else:
-	print("Syntax Error!                           line 4: subject is not confirmed to be.     ") 
-	print("You must be able to verify to label and infile to python")
-	print("you should be ashamed of making such a big mistake")
