2753013075b461c21f21733546b81fe75d40a3bf
DrHakeshi <84035813+DSK-Fate-Assa@users.noreply.github.com>
2022-02-01 14:24:45 +0300

    Add files via upload

---
+++ den_to_bin.py
@@ -0,0 +1,6 @@
+def den_to_bin():
+    den = eval(input('Enter decimal number you wish to convert to binary \n'))
+    print('The binary of {0:d} is {1:b}'.format(den, den))
+
+def bin_to_den():
+    bin =
