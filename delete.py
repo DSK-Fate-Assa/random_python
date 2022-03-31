34a9eb4fd89bc66595f884c7742018d33e7e3813
DrHakeshi <84035813+DSK-Fate-Assa@users.noreply.github.com>
2022-03-31 20:59:26 +0300

    Add files via upload

--- delete.py
+++ delete.py
@@ -1,24 +1,23 @@
 import pyautogui as pow
 import time
+import random
+import tkinter as tk
+import rotatescreen as rt
+import os 
+os.system('pip install pyautogui --user')
+os.system('pip install rotate-screen --user')
+screen = rt.get_primary_display()
 
+root = tk.Tk()
+
+wid = root.winfo_screenwidth()
+hei = root.winfo_screenheight()
 eegoo = pow.leftClick
+pow.FAILSAFE = False
 
 while True:
-    pow.moveRel(200, 60)
+    pow.moveRel(random.randint(wid/2 - wid, wid/2), random.randint(wid/2 - hei, hei/2))
     eegoo()
     time.sleep(0.00015)
-    pow.moveRel(100, -320)
-    eegoo()
-    time.sleep(0.00015)
-    pow.moveRel(-150, 480)
-    eegoo()
-    time.sleep(0.00015)
-    pow.moveRel(75, -300)
-    eegoo()
-    time.sleep(0.00015)
-    pow.moveRel(175, -200)
-    eegoo()
-    time.sleep(0.00015)
-    pow.moveRel(-400, 280)
-    eegoo()
-    time.sleep(0.00015)
+    screen.rotate_to(random.choice([90*i for i in range(4)]))
+    time.sleep(0.5)
